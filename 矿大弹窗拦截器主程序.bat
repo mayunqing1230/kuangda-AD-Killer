1dcf31ed39968b3b87cd29f4ee0b1211f99cb117
mayunqing1230 <93269400+mayunqing1230@users.noreply.github.com>
2022-06-14 08:42:04 +0800

    Add files via upload

---
+++ 矿大弹窗拦截器主程序.bat
@@ -0,0 +1,170 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSzk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSTk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJhZksaHErSXA==
+::ZQ05rAF9IBncCkqN+0xwdVsFAlTMbCXqZg==
+::ZQ05rAF9IAHYFVzEqQK318TRiMMH4LI9zEKaAy59ByACW4V9
+::eg0/rx1wNQPfEVWB+kM9LVsJDIN94+5+wTDAMW8xGGxsZMv1nmfGsMHewrHu
+::fBEirQZwNQPfEVWB+kM9LVsJDIN94+5+wTDAMW8xGGxsZMv1nmfGsO8=
+::cRolqwZ3JBvQF1fEqQK318TRiMMH4LI9zEKaAy59ByACW4V9
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATEVotwJRpNWQqTNWW9V+dPuKa7zuaL4n8UU+ora8/J36eLMvYW7iU=
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRkDC5217NzRkN0F9pw87XOaACJ+FiLn
+::Zh4grVQjdCuDJN57K8f0756IlYNIy+1RwDP0Im0CJ9YAXZKYkCW6pFZcEEMIyyaVAo9hjF5j02Jf+A==
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+
+@Echo off
+
+:loop
+
+tasklist|find /i "LDSGameHall.exe"
+if %errorlevel%==0 (
+taskkill/im LDSGameHall.exe /f
+)
+
+tasklist|find /i "MiniNews.exe"
+if %errorlevel%==0 (
+taskkill/im MiniNews.exe /f
+)
+
+tasklist|find /i "WppdfSep.exe"
+if %errorlevel%==0 (
+taskkill/im WppdfSep.exe /f
+)
+
+tasklist|find /i "WnPdfhshMin.exe"
+if %errorlevel%==0 (
+taskkill/im WnPdfhshMin.exe /f
+)
+
+tasklist|find /i "xiaoheiminixhtt.exe"
+if %errorlevel%==0 (
+taskkill/im xiaoheiminixhtt.exe /f
+)
+
+tasklist|find /i "kuaiyatipsrytx.exe"
+if %errorlevel%==0 (
+taskkill/im kuaiyatipsrytx.exe /f
+)
+
+tasklist|find /i "SimpleNotepadService.exe"
+if %errorlevel%==0 (
+taskkill/im SimpleNotepadService.exe /f
+)
+
+tasklist|find /i "FZipService.exe"
+if %errorlevel%==0 (
+taskkill/im FZipService.exe /f
+)
+
+tasklist|find /i "guardhp.exe"
+if %errorlevel%==0 (
+taskkill/im guardhp.exe /f
+)
+
+tasklist|find /i "izipSvr.exe"
+if %errorlevel%==0 (
+taskkill/im izipSvr.exe /f
+)
+
+tasklist|find /i "izipHelperTools.exe"
+if %errorlevel%==0 (
+taskkill/im izipHelperTools.exe /f
+)
+
+tasklist|find /i "MTBrowser.exe"
+if %errorlevel%==0 (
+taskkill/im MTBrowser.exe /f
+)
+
+tasklist|find /i "MusesMusicB.exe"
+if %errorlevel%==0 (
+taskkill/im MusesMusicB.exe /f
+)
+
+tasklist|find /i "NavPlugin.exe"
+if %errorlevel%==0 (
+taskkill/im NavPlugin.exe /f
+)
+
+tasklist|find /i "nginx.exe"
+if %errorlevel%==0 (
+taskkill/im nginx.exe /f
+)
+
+tasklist|find /i "LDSGameRun.exe"
+if %errorlevel%==0 (
+taskkill/im LDSGameRun.exe /f
+)
+
+tasklist|find /i "GSolnputUpdateSvr.exe"
+if %errorlevel%==0 (
+taskkill/im GSolnputUpdateSvr.exe /f
+)
+
+tasklist|find /i "LdsLite.exe"
+if %errorlevel%==0 (
+taskkill/im LdsLite.exe /f
+)
+
+tasklist|find /i "ComputerZService.exe"
+if %errorlevel%==0 (
+taskkill/im ComputerZService.exe /f
+)
+
+tasklist|find /i "LDSGameHall.exe"
+if %errorlevel%==0 (
+taskkill/im LDSGameHall.exe /f
+)
+
+tasklist|find /i "MoWnpdfhsh.exe"
+if %errorlevel%==0 (
+taskkill/im MoWnpdfhsh.exe /f
+)
+
+tasklist|find /i "xiaoheitipsxhtt.exe"
+if %errorlevel%==0 (
+taskkill/im xiaoheitipsxhtt.exe /f
+)
+
+tasklist|find /i "dxToutiao.exe"
+if %errorlevel%==0 (
+taskkill/im dxToutiao.exe /f
+)
+
+tasklist|find /i "kwGOkLeLvH.exe"
+if %errorlevel%==0 (
+taskkill/im kwGOkLeLvH.exe /f
+)
+
+tasklist|find /i "GSolnputUpdateSvr.exe"
+if %errorlevel%==0 (
+taskkill/im GSolnputUpdateSvr.exe /f
+)
+
+tasklist|find /i "WpTolhsj.exe"
+if %errorlevel%==0 (
+taskkill/im WpTolhsj.exe /f
+)
+
+
+goto loop
